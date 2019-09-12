0289025a925fd7ca44a472dfd7aa886ab80effc8
twinh <twinhuang@qq.com>
2019-09-12 17:33:18 +0800

    ci: fix TypeError: Unable to require `.d.ts` file

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
+  // Allow /node_modules/ for CI testing
+  transformIgnorePatterns: [],
+  // Fix TypeError: Unable to require `.d.ts` file.
+  // https://github.com/kulshekhar/ts-jest/issues/950
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    },
+  },
 };
