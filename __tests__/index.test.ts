b4d5f3e6197f1f08e3a3d2476f3a5a10ed3710e3
twinh <twinhuang@qq.com>
2021-03-29 16:48:53 +0800

    test: add commitFile test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,6 +1,7 @@
 import {logMessage, clearMessage, createRepo} from '..';
 import {Git} from "git-cli-wrapper";
 import * as npmlog from "npmlog";
+import * as fs from "fs";
 
 describe('Gitsync test package', () => {
   test('createRepo', async () => {
@@ -19,5 +20,14 @@ describe('Gitsync test package', () => {
 
     clearMessage();
     expect(logMessage()).not.toContain('message');
+  });
+
+  test('commitFile', async () => {
+    const repo = await createRepo();
+    await repo.commitFile('test.txt');
+
+    expect(fs.existsSync(repo.getFile('test.txt'))).toBeTruthy();
+
+    expect(await repo.run(['log', '-n', '1'])).toContain('add test.txt');
   });
 });
